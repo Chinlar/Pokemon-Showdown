cddcc1994f464db8c9ded2db794d3bbebb56d3df
Hydreigo <skyrims@icloud.com>
2014-12-06 13:56:22 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -139,6 +139,7 @@ exports.herokuhack = false;
 // displayed in the client.
 exports.customavatars = {
 	//'userid': 'customavatar.png'
+	//'jinthecat': 'jin.png'
 };
 
 // appealurl - specify a URL containing information on how users can appeal
